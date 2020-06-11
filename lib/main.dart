20d632021b979dd2f3bbe79b6b5c4132681911a1
RohitRajP <RohitRajP>
2020-06-11 12:53:24 +0530

    Fixed the theme issue

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,19 @@
-import 'package:openbeatsmobile/models/app/theme.dart';
-
 import './screens/index.dart' as indexScreen;
 import './imports.dart';
 
-void main() => runApp(MyApp());
+void main() => runApp(
+      ChangeNotifierProvider<ApplicationTheme>(
+        create: (_) => ApplicationTheme(),
+        child: MyApp(),
+      ),
+    );
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: "OpenBeats",
+      theme: Provider.of<ApplicationTheme>(context).getCurrentTheme(),
       home: indexScreen.IndexScreen(),
     );
   }
--- lib/models/app/theme.dart
+++ lib/models/app/theme.dart
@@ -1,9 +1,14 @@
 import '../../imports.dart';
 
 class ApplicationTheme extends ChangeNotifier {
-  ThemeData themeData = ThemeData(
+  ThemeData _themeData = ThemeData(
     brightness: Brightness.dark,
     primaryColor: Color(0xFFF32C2C),
     accentColor: Colors.redAccent,
   );
+
+  // used to get the current theme
+  ThemeData getCurrentTheme() {
+    return _themeData;
+  }
 }
