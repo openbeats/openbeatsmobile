259448323d76e50f94594b31b59d0b1867ab0081
RohitRajP <RohitRajP>
2020-06-12 09:33:38 +0530

    Added bottomNav bar and made it depend on and update provider

--- lib/imports.dart
+++ lib/imports.dart
@@ -4,3 +4,5 @@ export 'package:provider/provider.dart';
 export 'package:openbeatsmobile/models/app/theme.dart';
 export 'package:flutter/services.dart';
 export 'package:sliding_up_panel/sliding_up_panel.dart';
+export 'package:openbeatsmobile/models/app/state.dart';
+export 'package:openbeatsmobile/components/bottomNavDestinations.dart';
--- lib/screens/index.dart
+++ lib/screens/index.dart
@@ -8,9 +8,34 @@ class IndexScreen extends StatefulWidget {
 class _IndexScreenState extends State<IndexScreen> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: SafeArea(
-        child: _indexPageBody(),
+    print("==============BUILT INDEX============-");
+    return ChangeNotifierProvider<ApplicationState>(
+      create: (_) => ApplicationState(),
+      child: Scaffold(
+        body: SafeArea(
+          child: _indexPageBody(),
+        ),
+        bottomNavigationBar: _bottomNavBar(),
+      ),
+    );
+  }
+
+  Widget _bottomNavBar() {
+    return Consumer<ApplicationState>(
+      builder: (context, data, _) => BottomNavigationBar(
+        onTap: (tappedIndex) =>
+            Provider.of<ApplicationState>(context, listen: false)
+                .setBottomNavBarCurrentIndex(tappedIndex),
+        currentIndex: Provider.of<ApplicationState>(context)
+            .getBottomNavBarCurrentIndex(),
+        backgroundColor:
+            Provider.of<ApplicationTheme>(context).getBottomAppBarColor(),
+        items: allDestinations.map((Destination destination) {
+          return BottomNavigationBarItem(
+              icon: Icon(destination.icon),
+              backgroundColor: destination.color,
+              title: Text(destination.title));
+        }).toList(),
       ),
     );
   }
