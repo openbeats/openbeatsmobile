08a5083f8e953c80fa264338466147a621d2051d
RohitRajP <RohitRajP>
2020-06-16 23:40:27 +0530

    Added globalTheme data

---
+++ lib/globals/globalThemes.dart
@@ -0,0 +1,34 @@
+import 'package:obsmobile/imports.dart';
+
+class GlobalThemes {
+  // holds the theme data for the application
+  ThemeData _themeData = ThemeData(
+    brightness: Brightness.dark,
+    scaffoldBackgroundColor: Color(0xFF14161C),
+    appBarTheme: AppBarTheme(
+        color: Colors.transparent,
+        elevation: 0.0,
+        textTheme: TextTheme(
+            headline6: TextStyle(
+          fontWeight: FontWeight.bold,
+          fontSize: 30.0,
+        ))),
+    primaryColor: Color(0xFFF32C2C),
+    accentColor: Colors.redAccent,
+    bottomAppBarColor: Color(0xFF212229),
+  );
+
+  // holds the theme data for the toast messages
+  Map<String, dynamic> _toastThemeData = {
+    "backgroundColor": Colors.blue,
+    "position": ToastPosition.bottom,
+    "duration": Duration(seconds: 5),
+    "textPadding": EdgeInsets.all(20.0),
+    "dismissOtherOnShow": true,
+  };
+
+  // used to get the app theme data
+  ThemeData getAppTheme() => _themeData;
+  // used to get the toast theme data
+  Map<String, dynamic> getToastTheme() => _toastThemeData;
+}
