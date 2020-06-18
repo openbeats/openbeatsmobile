ae8752fd0449187c39267431ed49c3594e8aa071
RohitRajP <RohitRajP>
2020-06-18 06:43:37 +0530

    Added proper error handing and also solved the SnackBar showing problem when typing

--- lib/globals/globalVars.dart
+++ lib/globals/globalVars.dart
@@ -2,11 +2,18 @@ import 'package:obsmobile/imports.dart';
 
 // controller for the SlidingUpPanel
 PanelController _slidingUpPanelController = new PanelController();
-
 // holds the openbeatsAPI endpoint
 String _apiEndpoint = "https://staging-api.openbeats.live";
+// holds the error message currently being shown by the snackbar (to prevent the annoying queuing)
+String _currentSnackBarErrorMessage;
 
 // used to get the SlidingUpPanel controller
 PanelController getSlidingUpPanelController() => _slidingUpPanelController;
 // used to get the openbeatsAPI endpoint
 String getApiEndpoint() => _apiEndpoint;
+// used to get the currScackBarErrorMessage 
+String getCurrSnackBarErrorMsg() => _currentSnackBarErrorMessage;
+
+// used to set the currentSnackBarErrorMessage
+void setCurrSnackBarErrorMsg(String value) => _currentSnackBarErrorMessage = value;
+
--- lib/services/networkOps.dart
+++ lib/services/networkOps.dart
@@ -1,13 +1,95 @@
 import 'package:obsmobile/imports.dart';
 
+// used to handle the exceptions raised by the network request methods
+void _handleExceptionsRaised(String exception, BuildContext context) {
+  // holds the string to show the user
+  String _userMessage;
+  // constructing the right error message
+  if (exception == "SocketException")
+    _userMessage =
+        "Unable to connect to the internet. Please check network connectivity";
+  else if (exception == "TimeoutException")
+    _userMessage = "Server took too long to respond. Please try again.";
+  else if (exception == "BadRequestException")
+    _userMessage =
+        "Application sending incorrect data. Please contact developer.";
+  else if (exception == "UnauthorizedException")
+    _userMessage =
+        "Unable to access forbidden data. Please try signing in again.";
+  else
+    _userMessage = "An unknown network error occurred. Please try again.";
+
+  // constructing snackbar to show error message
+  SnackBar _errorSnackBar = SnackBar(
+    content: Text(
+      _userMessage,
+      style: TextStyle(color: Colors.white),
+    ),
+    backgroundColor: Colors.red,
+    duration: Duration(seconds: 10),
+    action: SnackBarAction(
+        label: "Close",
+        textColor: Colors.white,
+        onPressed: () {
+          homePageScaffoldKey.currentState.hideCurrentSnackBar();
+        }),
+  );
+
+  // checking if snackbar should be shown
+  if (getCurrSnackBarErrorMsg() == null &&
+      getCurrSnackBarErrorMsg() != exception) {
+    // setting the current exception in global store
+    setCurrSnackBarErrorMsg(exception);
+    // hiding any present snackbars
+    homePageScaffoldKey.currentState.removeCurrentSnackBar();
+    // showing snackbar
+    homePageScaffoldKey.currentState
+        .showSnackBar(_errorSnackBar)
+        .closed
+        .then((value) => setCurrSnackBarErrorMsg(null));
+  }
+}
+
+// used to classify the responses recieved for a network request
+dynamic _returnResponse(Response response, BuildContext context) {
+  switch (response.statusCode) {
+    case 200:
+      var responseJson = json.decode(response.body.toString());
+      return {"status": true, "data": responseJson};
+    case 400:
+      _handleExceptionsRaised("BadRequestException", context);
+      return {"status": false, "error": "BadRequestException"};
+    case 401:
+    case 403:
+      _handleExceptionsRaised("UnauthorizedException", context);
+      return {"status": false, "error": "UnauthorizedException"};
+    case 500:
+    default:
+      _handleExceptionsRaised("UnknownException", context);
+      return {
+        "status": false,
+      };
+  }
+}
+
 // get search suggestions for SearchNowPage
 void getSearchSuggestion(BuildContext context) async {
-  // getting the current search string
-  String query = getCurrentSearchString();
-  // sending http request
-  var response = await get(getApiEndpoint() + "/suggester?k=" + query);
-  var responseJSON = json.decode(response.body);
-  // updating the seacch suggestions list
-  Provider.of<SearchTabModel>(context, listen: false)
-      .updateSearchSuggestions(responseJSON["data"]);
+  try {
+    // getting the current search string
+    String query = getCurrentSearchString();
+    // sending http request
+    var response = await get(getApiEndpoint() + "/suggester?k=" + query);
+    var responseClassified = _returnResponse(response, context);
+    if (responseClassified["status"] == true) {
+      // updating the seacch suggestions list
+      Provider.of<SearchTabModel>(context, listen: false)
+          .updateSearchSuggestions(responseClassified["data"]["data"]);
+    }
+  } on SocketException {
+    // no internet connection
+    _handleExceptionsRaised("SocketException", context);
+  } on TimeoutException {
+    // timeout exception
+    _handleExceptionsRaised("TimeoutException", context);
+  }
 }
