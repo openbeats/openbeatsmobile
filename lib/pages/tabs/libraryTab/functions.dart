d74738f55576fc6dc40f68ed071f49d152cbf64c
RohitRajP <RohitRajP>
2020-06-29 17:39:47 +0530

    Fixed the loading start on playlistView start

--- lib/pages/tabs/libraryTab/functions.dart
+++ lib/pages/tabs/libraryTab/functions.dart
@@ -3,6 +3,8 @@ import 'package:obsmobile/imports.dart';
 // used to navigate to playlistView page
 void navigateToPlaylistView(
     BuildContext context, Map<String, String> playlistData) {
+  // setting loading indicator
+  Provider.of<PlaylistViewData>(context, listen: false).setIsLoading(true);
   // navigate to the playlist view page
   Navigator.of(context).pushNamed('/playlistView', arguments: playlistData);
 }
